42d1616b769381ad07c6f32719b3987c7ea031b2
Toriku <anatoly.kravtsov@gmail.com>
2021-02-18 13:55:46 +0500

    feat: create Dream Team done

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let trimmed_arr = []
+  let output = "";
+
+  for (let member of members) {
+    if (typeof member === "string") {
+      member = member.trim()
+      trimmed_arr.push(member.trim()[0].toUpperCase());
+    }
+
+  }
+  trimmed_arr = trimmed_arr.sort();
+
+  for (let letter of trimmed_arr) {
+    output += letter;
+  }
+  return output;
 };
